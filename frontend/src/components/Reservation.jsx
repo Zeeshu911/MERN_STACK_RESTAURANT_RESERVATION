bfbb82bee74c35860e6ab125ce94c17fa3d90169
Zeeshu911 <117258902+Zeeshu911@users.noreply.github.com>
2024-02-13 11:22:49 +0500

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://confused-deer-swimsuit.cyclic.app/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
