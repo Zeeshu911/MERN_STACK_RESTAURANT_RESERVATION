a2f35d475047025ef461f129ae22f9a464929e4d
Zeeshu911 <117258902+Zeeshu911@users.noreply.github.com>
2024-02-13 12:33:37 +0500

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://mern-stack-restaurant-reservation-backend/reservation/send",
+        "http://localhost:4000/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
