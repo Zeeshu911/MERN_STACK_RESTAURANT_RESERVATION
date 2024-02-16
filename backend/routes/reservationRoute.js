763083f52f7502b418fad8e07c336e804fa302fe
Zeeshu911 <117258902+Zeeshu911@users.noreply.github.com>
2024-02-16 20:58:43 +0500

    Update reservationRoute.js

--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import send_reservation from "../controller/reservation.js";
+import send_reservation,{getRequest} from "../controller/reservation.js";
 
 const router = express.Router();
 
 router.post("/send", send_reservation);
+router.post("/message", getRequest);
 
 export default router;
