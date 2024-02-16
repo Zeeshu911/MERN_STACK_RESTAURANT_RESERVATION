ec7ae9eb830f247bdda5bf2d48e1aa57c0c72a73
Zeeshu911 <117258902+Zeeshu911@users.noreply.github.com>
2024-02-16 20:57:58 +0500

    Update reservation.js

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -26,7 +26,7 @@ const send_reservation = async (req, res, next) => {
   }
 };
 
-const getRequest = async(req,res,next)=>{
+export const getRequest = async(req,res,next)=>{
   res.status(200).json({
     success: true,
     message: "GET REQUEST WORKING"
